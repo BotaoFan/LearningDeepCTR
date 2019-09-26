fbe60dc87978fd046b6b52415632782efce4e015
Botao Fan <botaofan@BotaodeiMac.local>
2019-09-26 22:35:36 +0800

    Initialization

---
+++ DeepFM/DeepFM.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+# @Time : 2019/9/26
+# @Author : Botao Fan
+
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
---
+++ DeepFM/__init__.py
@@ -0,0 +1,3 @@
+#-*- coding:utf-8 -*-
+# @Time : 2019/9/26
+# @Author : Botao Fan
