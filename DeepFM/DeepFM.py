35ec6d6023cd77b0b1f6b761a4dc743fe4191ae1
fan <1297198416@qq.com>
2019-09-27 18:12:50 +0800

    Nothing

--- DeepFM/DeepFM.py
+++ DeepFM/DeepFM.py
@@ -13,75 +13,89 @@ from sklearn.metrics import roc_auc_score
 from time import time
 from tensorflow.contrib.layers.python.layers import batch_norm as batch_norm
 
+
 class DeepFM(BaseEstimator, TransformerMixin):
-    def __init__(self, feature_size, field_size,
-                 embedding_size=8, drop_fm=[1.0, 1.0],
-                 deep_layers=[32, 32], drop_deep=[0.5, 0.5, 0.5],
-                 deep_layers_activation=tf.nn.relu,
-                 epoch=10, batch_size=256,
-                 learning_rate=0.001, optimizer_type='adam',
-                 batch_norm=0, batch_norm_decay=0.995,
-                 verbose=False, random_seed=42,
-                 use_fm=True,use_deep=True,
-                 loss_type='logloss', eval_metric=roc_auc_score,
-                 l2_reg=0, greater_is_better=True):
-        assert (use_fm or use_deep)
-        assert loss_type in ['logloss', 'mse'], \
-            "loss_type can be either 'logloss' for classification task or 'mse' for regression task"
-        self.feature_size = feature_size
-        self.field_size = field_size
-        self.embedding_size = embedding_size
-        self.drop_fm = drop_fm
-        self.drop_deep = drop_deep
+    def __init_(self, feature_size, field_size,
+                embedding_size=8, dropout_fm=[1, 1],
+                deep_layers=[32, 32],  dropout_deep=[0.5, 0.5, 0.5],
+                deep_layers_activation=tf.nn.relu,
+                learning_rate=0.001, optimizer_type='adam',
+                batch_norm=0, batch_norm_decay=0.995,
+                verbose=False, random_seed=2016,
+                use_fm=True, use_deep=True,
+                loss_type='log_loss', eval_metric=roc_auc_score,
+                l2_reg=0.0, greater_is_better=True):
+        self.feature_size = feature_size  # M : number of all features(after one-hot)
+        self.field_size = field_size  # F : number of all fields(before one-hot)
+        self.embedding_size = embedding_size  # E : number of embedding
+        self.dropout_fm = dropout_fm
         self.deep_layers = deep_layers
-        self.deep_layers_activation=deep_layers_activation
-        self.epoch = epoch
-        self.batch_size = batch_size
+        self.dropout_deep = dropout_deep
+        self.deep_layers_activation = deep_layers_activation
         self.learning_rate = learning_rate
         self.optimizer_type = optimizer_type
         self.batch_norm = batch_norm
         self.batch_norm_decay = batch_norm_decay
-        self.verbose = verbose
-        self.random_seed = random_seed
         self.use_fm = use_fm
         self.use_deep = use_deep
         self.loss_type = loss_type
         self.eval_metric = eval_metric
         self.l2_reg = l2_reg
         self.greater_is_better = greater_is_better
+        self.verbose = verbose
+        self.random_seed = random_seed
+        self.weights = self._init_weights()
 
-        self._init_graph()
-
-    def _init_graph(self):
-        pass
-
-    def _initialize_weight(self):
+    def _weights(self):
         weights = dict()
-        #FM Layer
-        weights['feature_embedding'] = tf.Variable(
-            tf.random_normal([self.feature_size, self.embedding_size], 0.0, 0.01),
-            name='feature_embedding')
-        weights['feature_bias'] = tf.Variable(
-            tf.random_uniform([self.feature_size, 1], 0.0, 0.01),
-            name='feature_bias'
-        )
-        #Deep Layers
-        layers_num = len(self.deep_layers)
-        input_num = self.field_size * self.embedding_size
-        glorot = np.sqrt(2.0 / (input_num + self.deep_layers[0]))
-        weights['layer_0'] = tf.Variable(
-            np.random.normal(loc=0.0, scale=glorot, size=[input_num, self.deep_layers[0]], dtype=np.float32))
-        weights['bias_0'] = tf.Variable(
-            np.random.normal(loc=0.0, scale=glorot, size=[1, self.deep_layers[0]], dtype=np.float32)
-        )
-
-        for i in range(1, layers_num):
-            glorot = np.sqrt(2.0 / (layers_num[i] + layers_num[i - 1]))
+        #embedding
+        weights['embedding_weights'] = tf.Variable(tf.random.normal([self.feature_size, self.embedding_size], 0.0, 0.01),
+                                                   name='embedding_weights')
+        weights['embedding_bias'] = tf.Variable(tf.random.uniform([self.feature_size, 1], 0.0, 1.0),
+                                                name='embedding_bias')
+        #deep layers
+        input_size = self.field_size * self.embedding_size + self.deep_layers[0]
+        glorot = np.sqrt(2.0 / (input_size))
+        weights['layer_0'] = tf.Variable(np.random.normal(loc=0.0, scale=glorot, size=(input_size, self.deep_layers[0])))
+        weights['bias_0'] = tf.Variable(np.random.normal(loc=0.0, scale=glorot, size=(1, self.deep_layers[0])))
+        for i in range(1, len(self.deep_layers)):
+            glorot = np.sqrt(2.0 / (self.deep_layers[i-1] + self.deep_layers[i]))
             weights['layer_%d' % i] = tf.Variable(
-                np.random.normal(loc=0.0, scale=glorot, size=[layers_num[i-1], layers_num[i]], dtype=np.float32))
+                np.random.normal(loc=0.0, scale=glorot, size=[self.deep_layers[i-1], self.deep_layers[i]], dtype=np.float32))
             weights['bias_%d' % i] = tf.Variable(
                 np.random.normal(loc=0.0, scale=glorot, size=[1, self.deep_layers[i]], dtype=np.float32)
             )
+        if self.use_fm and self.use_deep:
+            input_size = self.field_size + self.embedding_size + self.deep_layers[-1]
+        elif self.use_fm:
+            input_size = self.field_size + self.embedding_size
+        elif self.use_deep:
+            input_size = self.deep_layers[-1]
+
+        glorot = np.sqrt(2.0 / input_size)
+        weights['concat_weight'] = tf.Variable(np.random.normal(loc=0.0, scale=glorot, size=(input_size, 1), dtype=np.float32))
+        weights['concat_bias'] = tf.Variable(tf.constant(0.01), dtype=np.float32)
+        return weights
+
+    def _init_graph(self):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            tf.set_random_seed(self.random_seed)
+            #Initialize placeholder
+            self.feat_index = tf.placeholder(tf.float32, shape=[None, None], name='feat_index')
+            self.feat_value = tf.placeholder(tf.float32, shape=[None, None], name='feat_value')
+            self.label = tf.placeholder(tf.float32, shape=[None, 1], name='label')
+            self.dropout_keep_fm = tf.placeholder(tf.float32, shape=[None], name='dropout_keep_fm')
+            self.dropout_keep_deep = tf.placeholder(tf.float32, shape=[None], name='dropout_keep_deep')
+
+            #Embedding
+            self.embedding = tf.nn.embedding_lookup(self.weights['embedding_weights'], self.feat_index)
+            self.embedding = tf.
+
+
+
+
+
 
 
 
